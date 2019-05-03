dc78cb79d1078a244de7dc0d78b2423db02d1796
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-03 19:34:14 +0300

    add build

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,104 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var server = require('browser-sync').create();
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const del = require('del');
 
-gulp.task('css', function () {
-  return gulp.src('source/sass/style.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('source/css'))
-    .pipe(server.stream());
+const paths = {
+  styles: {
+    src: 'source/sass/style.scss',
+    dest: 'build/css/'
+  },
+  html: {
+    src: 'source/*.html',
+    dest: 'build/'
+  },
+  images: {
+    src: 'source/img/**/*',
+    dest: 'build/img/'
+  },
+  fonts: {
+    src: 'source/fonts/**/*',
+    dest: 'build/fonts/'
+  },
+  scripts: {
+    src: 'source/scripts/**/*.js',
+    dest: 'build/scripts/'
+  }
+};
+
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require("css-mqpacker");
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+
+gulp.task('style', () => {
+  return gulp.src(paths.styles.src).
+    pipe(plumber()).
+    pipe(sass()).
+    pipe(postcss([
+      autoprefixer({
+        browsers: [
+          'last 1 version',
+          'last 2 Chrome versions',
+          'last 2 Firefox versions',
+          'last 2 Opera versions',
+          'last 2 Edge versions'
+        ]
+      }),
+      mqpacker({sort: true})
+    ])).
+    pipe(sourcemaps.init()).
+    pipe(cleanCSS()).
+    pipe(sourcemaps.write()).
+    pipe(rename('style.min.css')).
+    pipe(gulp.dest(paths.styles.dest));
 });
 
-gulp.task('server', function () {
+
+const server = require('browser-sync').create();
+
+gulp.task('server', () => {
   server.init({
-    server: 'source/',
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
-  gulp.watch('source/*.html').on('change', server.reload);
+  gulp.watch(paths.styles.src, gulp.series('style'));
+  gulp.watch(paths.html.src, gulp.series('html')).
+    on('change', server.reload);
 });
 
-gulp.task('start', gulp.series('css', 'server'));
+gulp.task('scripts', (done) => {
+  done();
+});
+
+
+gulp.task('html', () => {
+  return gulp.src(paths.html.src).
+    pipe(gulp.dest(paths.html.dest));
+});
+
+gulp.task('images', () => {
+  return gulp.src(paths.images.src).
+    pipe(gulp.dest(paths.images.dest));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src(paths.fonts.src).
+    pipe(gulp.dest(paths.fonts.dest));
+});
+
+gulp.task('copy', gulp.parallel('html', 'scripts', 'style', 'images', 'fonts'));
+
+gulp.task('clean', () => del(['build']));
+
+gulp.task('build', gulp.series('clean', 'copy'));
+
+gulp.task('start', gulp.series('build', 'server'));
