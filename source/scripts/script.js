120c76259f2fd1f8747863266db2e6f06ead80e3
Andrew Zakharchenko <drewtsure@gmail.com>
2019-05-07 21:38:28 +0300

    return of some code

---
+++ source/scripts/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var navigation = document.querySelector('.navigation');
+var navigationButton = navigation.querySelector('.navigation__button');
+var menu = navigation.querySelector('.navigation__list');
+
+navigationButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  console.log('click');
+  menu.classList.toggle('navigation__list--open');
+  navigationButton.classList.toggle('navigation__button--close');
+});
