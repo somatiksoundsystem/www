db408fdc86ca2d4923fb7b49c237123ce0e462d0
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-25 13:58:57 +0300

    Updates

---
+++ artist-page/scripts/navigation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var navigation = document.querySelector('.navigation');
+var navigationButton = navigation.querySelector('.navigation__button');
+var menu = navigation.querySelector('.navigation__list');
+
+navigationButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle('navigation__list--open');
+  navigationButton.classList.toggle('navigation__button--open');
+});
