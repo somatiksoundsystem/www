2bf48fc373b24a0877d894385040dbee334d0b1c
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-05-31 20:24:11 +0300

    more mappers

--- generate/db-query.js
+++ generate/db-query.js
@@ -1,5 +1,20 @@
 const {ARTISTS, ALBUMS} = require('./artists-data');
 
+const entries = (only = ['name', 'nickname', 'photo_url', 'info']) => (it) =>
+  Object.entries(it).filter(([key]) => only.includes(key))
+    .map(([key, value]) => `'${key}' => '${value}'`).join(`,\n`);
+
+console.log(`createMany([
+            ${ARTISTS.map((it) => `[${entries()(it)}]`).join(',\n')}
+        ]);
+`);
+
+console.log(
+  `createMany([
+            ${ALBUMS.map((it) => `[${entries(['name', 'date', 'url', 'image'])(it)}]`).join(',\n')}
+        ]);
+`);
+
 console.log(ALBUMS.map((it) => `('${it.name}',TIMESTAMP(STR_TO_DATE('${it.date ? it.date : '01.01.2016'}','%d.%m.%Y')),'${it.image}',${it.url ? `'${it.url}'` : 'DEFAULT'})`)
   .join(`,\n`));
 
@@ -26,9 +41,9 @@ const resolveAlbum = (it) => {
 ARTISTS.forEach(resolveAlbum)
 
 ALBUMS.forEach((it) => {
-  console.log(`$album = Album::resolve("${it.name}");`)
+  console.log(`$album = Album::resolve("${it.name}");`);
   Object.entries(it.social || {}).forEach(([key, value]) => {
     console.log(`$album->addSocialLink("${key}", "${value}");`)
-  })
-})
+  });
+});
 
