cb3333a0bb63d9a1bf9565828642a53985654445
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-14 16:23:10 +0300

    generate shop

---
+++ generate/shop-data.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const generateId = require('./mapper.js');
+const {checkUnique} = require('./utils.js');
+
+const SHOP_ITEM_PATH = `/shop/`;
+const SHOP_ITEM_PHOTO_PATH = `/img/shop/thumb/`;
+
+const DEFAULT_SHOP_ITEM_INFO = {
+  photo: `default.png`,
+  name: `Super Switshot`,
+  info: `Тёплый мягкий охуенный и суперпиздатый свитшот, бери его скорее сука!`,
+};
+
+const checkUniqueName = checkUnique(
+  ({id, name}) => {
+    throw new Error(`Duplicate id [${id}] for name: '${name}'`)
+  }
+);
+
+const newShopItem = (name, info = DEFAULT_SHOP_ITEM_INFO) => {
+  const id = generateId(name);
+  const href = SHOP_ITEM_PATH + id;
+  const photo_url = info.photo;
+  const photo = SHOP_ITEM_PHOTO_PATH + info.photo;
+  const item = {...info, ...{id, name, href, photo, photo_url}};
+  checkUniqueName(item);
+  return item;
+};
+
+const SHOP_ITEMS = [
+  newShopItem(`Кепка Соматик`),
+  newShopItem(`Кофта Соматик`, {
+    info: `Моднейший`,
+  }),
+  newShopItem(`Часы Соматик`),
+  newShopItem(`Сома-трусы`),
+];
+
+module.exports = {SHOP_ITEMS};
--- generate/write-data.js
+++ generate/write-data.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const log = require('fancy-log');
-const fs = require('fs');
 const util = require('util');
+const writeFile = util.promisify(require('fs').writeFile);
 const artists = require('./artists-data').ARTISTS;
-
-const writeFile = util.promisify(fs.writeFile);
+const shop = require('./shop-data').SHOP_ITEMS;
 
 const root = process.cwd();
 
@@ -19,11 +18,14 @@ const reduceArtist = (acc, it) => {
   return acc;
 };
 
-const artistsMap = artists.reduce(reduceArtist, {});
+function write(path, data) {
+  writeFile(path, JSON.stringify(data, null, 2), `utf8`)
+    .then(() => log(`Successfully saved to: ${path}`))
+    .catch((e) => log(`Failed to save file with ${e.message}`));
+}
 
-const artistJson = `../data/artists.json`;
-writeFile(artistJson, JSON.stringify(artistsMap, null, 2), `utf8`)
-  .then(() => log(`Successfully saved to: ${artistJson}`))
-  .catch((e) => log(`Failed to save file with ${e.message}`));
+write(`../data/artists.json`, artists.reduce(reduceArtist, {}));
 
+const shopMap = shop.reduce((acc, it) =>({[it.id]: it, ...acc}), {});
+write(`../data/shop.json`, shopMap);
 
